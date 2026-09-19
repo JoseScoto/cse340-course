373d217b22343079ea46a05776813d9c52490365
JoseScoto <jescotoarana@byupathway.edu>
2026-09-18 20:46:39 -0600

    Application updated. Route and query paramenters were added.

--- src/models/projects.js
+++ src/models/projects.js
@@ -22,4 +22,24 @@ const getAllProjects = async () => {
     return result.rows;
 }
 
-export { getAllProjects }
+const getProjectsByOrganizationId = async (organizationId) => {
+    const query = `
+        SELECT
+          project_id,
+          organization_id,
+          name AS title,
+          description,
+          location,
+          start_date AS date
+        FROM service_project
+        WHERE organization_id = $1
+        ORDER BY start_date;
+      `;
+
+    const queryParams = [organizationId];
+    const result = await db.query(query, queryParams);
+
+    return result.rows;
+};
+
+export { getAllProjects, getProjectsByOrganizationId };
